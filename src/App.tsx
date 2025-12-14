d514930ef3e00b897d13fb4f71ae855b77002817
IrtazaAmjad0412 <irtaza0412@gmail.com>
2025-12-13 19:25:33 -0500

    feat: remove HomePage component and styles; add new Home and Shop pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { HomePage } from "./components/HomePage/HomePage";
 import "./App.css";
 
 function App() {
   return (
-    <div className="app-container">
-      <HomePage />
-      {/* <ShopPage /> */}
-      {/* <CartPage /> */}
-    </div>
+    <div className="app-container"></div>
+    //
   );
 }
 
---
+++ src/pages/Shop.tsx
@@ -0,0 +1,17 @@
+export const Shop = () => {
+  // const [product, setProduct] = useState<Product[]>([]);
+
+  // useEffect(() => {
+  //   fetchAllProducts()
+  //     .then((data) => {
+  //       console.log(data);
+  //       setProduct(data);
+  //     })
+  //     .catch((err) => console.error("Error fetching Products:", err));
+  // }, []);
+
+  return (
+    <div>Shop Page</div>
+    //
+  );
+};
