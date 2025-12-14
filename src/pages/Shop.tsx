fcdfeba2448ae7659582524a10b2631456aa01d7
IrtazaAmjad0412 <irtaza0412@gmail.com>
2025-12-14 16:15:50 -0500

    feat: implement product fetching and display in Shop component

--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -1,6 +1,30 @@
+import { useEffect, useState } from "react";
+import type { Product } from "../types/product";
+import { fetchAllProducts } from "../data/api";
+
 export const Shop = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetchAllProducts()
+      .then((data) => {
+        console.log(data);
+        setProducts(data);
+      })
+      .catch((err) => console.error("Error fetching Products:", err));
+    // Use .finally method to eventually set isLoading state
+  }, []);
+
   return (
-    <div>Shop Page</div>
-    //
+    <div>
+      Shop Page
+      {products.map((product) => (
+        <div key={product.id}>
+          <img src={product.image} alt={product.title} />
+          <p>{product.title}</p>
+          <p>{product.price}</p>
+        </div>
+      ))}
+    </div>
   );
 };
