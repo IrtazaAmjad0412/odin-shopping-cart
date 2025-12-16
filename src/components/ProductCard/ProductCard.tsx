f157e11dda271d8f98c7f0743b12d2999cae1590
IrtazaAmjad0412 <irtaza0412@gmail.com>
2025-12-15 23:32:34 -0500

    feat: update ProductCard styles and structure; add quantity control functionality

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { formatPrice } from "../../utils/utils";
 import type { ProductCardProp } from "../../types/props";
 import "./ProductCard.css";
@@ -8,6 +9,11 @@ export const ProductCard = ({ product }: ProductCardProp) => {
       <img src={product.image ?? ""} alt={product.title} />
       <p>{product.title}</p>
       <p>${formatPrice(product.price)}</p>
+      <div className="quantity-control">
+        <button type="button">-</button>
+        <input type="number" />
+        <button type="button">+</button>
+      </div>
       <button type="button">Add To Cart</button>
     </div>
   );
