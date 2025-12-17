017c6d21c11a4353f2e1989efad36ae876740ec0
IrtazaAmjad0412 <irtaza0412@gmail.com>
2025-12-16 19:34:43 -0500

    feat: enhance ProductCard with quantity control functionality and input handling

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,18 +1,56 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { formatPrice } from "../../utils/utils";
 import type { ProductCardProp } from "../../types/props";
 import "./ProductCard.css";
 
 export const ProductCard = ({ product }: ProductCardProp) => {
+  const [quantity, setQuantity] = useState<number | "">(1);
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    if (value === "") {
+      setQuantity("");
+      return;
+    }
+    const numericValue = Number(value);
+    if (!isNaN(numericValue)) {
+      setQuantity(Math.min(99, Math.max(1, numericValue)));
+    }
+  };
+
+  const handleOnBlur = () => {
+    if (quantity === "") {
+      setQuantity(1);
+    }
+  };
+
+  const increaseQuantity = () =>
+    setQuantity((quantity) => Math.min(99, Number(quantity) + 1));
+  const decreaseQuantity = () =>
+    setQuantity((quantity) => Math.max(1, Number(quantity) - 1));
+
   return (
     <div className="product-card-section">
       <img src={product.image ?? ""} alt={product.title} />
       <p>{product.title}</p>
       <p>${formatPrice(product.price)}</p>
       <div className="quantity-control">
-        <button type="button">-</button>
-        <input type="number" />
-        <button type="button">+</button>
+        <button
+          type="button"
+          onClick={decreaseQuantity}
+          disabled={quantity === 1 || quantity === ""}
+        >
+          -
+        </button>
+        <input
+          type="number"
+          value={quantity}
+          onChange={handleOnChange}
+          onBlur={handleOnBlur}
+        />
+        <button type="button" onClick={increaseQuantity} disabled={quantity === 99}>
+          +
+        </button>
       </div>
       <button type="button">Add To Cart</button>
     </div>
